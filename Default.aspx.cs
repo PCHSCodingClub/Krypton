cad36e755d7436af64b7809a71f16b745058d59c
Darokrithia <dieltabin@gmail.com>
2017-01-31 23:32:58 -0700

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,9 +1,22 @@
 using System;
 using System.Web;
 using System.Web.UI;
+
 namespace Krypton
 {
+
 	public partial class Default : System.Web.UI.Page
 	{
+		public void generateCards(object sender, EventArgs args)
+		{
+			Random rand = new Random(20);
+
+			card1.Text = rand.Next().ToString();
+			card2.Text = rand.Next().ToString();
+			card3.Text = rand.Next().ToString();
+			card4.Text = rand.Next().ToString();
+			card5.Text = rand.Next().ToString();
+			card6.Text = rand.Next().ToString();
+		}
 	}
 }

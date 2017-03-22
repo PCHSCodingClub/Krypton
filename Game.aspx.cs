6290397d1ddf34f01ba44d8b7a894dd1b1d7a0ab
Darokrithia <dieltabin@gmail.com>
2017-03-21 19:37:38 -0600

    Update Game.aspx.cs

--- Game.aspx.cs
+++ Game.aspx.cs
@@ -1,39 +1,23 @@
 using System;
 using System.Collections;
 using System.Data;
-using System.Diagnostics;
-using System.Threading;
-using System.ComponentModel;
 
-namespace Krypton
-{
+namespace Krypton {
 
-	public partial class Default : System.Web.UI.Page
-	{
-		DataTable dt = new DataTable();             //used to turn equations into ints
-		Random rand = new Random();                 //generates random numbers
+	public partial class Default : System.Web.UI.Page 	{
+		DataTable dt = new DataTable();				//used to turn equations into ints
+		Random rand = new Random();					//generates random numbers
 
-		int[] cards = new int[6];                   //stores the numbers used by cards
+		int[] cards = new int[6];					//stores the numbers used by cards
 
-		String answer;                              //the computed answer as a string (used later)
-		int computedAnswer;                         //the computed answer
-		int pointTotal;
+		String answer;								//the computed answer as a string (used later)
+		int computedAnswer;							//the computed answer
 
-		public void generateCards(object sender, EventArgs args)    //randomly generates 
-		{
-			for (int i = 0; i < cards.Length; i++)                  //makes a random number for each card
-			{
-				try
-				{
-					cards[i] = (1 + rand.Next((int)Session["MaxNumber"]));
-				}
-
-					catch
-				{
-					cards[i] = (1 + rand.Next(12));
-				}
+		public void generateCards(object sender, EventArgs args) {		//randomly generates 
+			for (int i = 0; i < cards.Length; i++) {					//makes a random number for each card
+				cards[i] = (1 + rand.Next((int) Session["maxNumber"]));
 			}
-			card1.Text = cards[0].ToString();                       //puts the cards onto the page.
+			card1.Text = cards[0].ToString();							//puts the cards onto the page.
 			card2.Text = cards[1].ToString();
 			card3.Text = cards[2].ToString();
 			card4.Text = cards[3].ToString();
@@ -41,41 +25,30 @@ namespace Krypton
 			card6.Text = cards[5].ToString();
 
 
-			ViewState.Add("canGetCards", true);                                 //allows points to be obtained again.
-			ViewState.Add("cards", cards);                          //stores data
+			ViewState.Add("canGetCards", true);									//allows points to be obtained again.
+			ViewState.Add("cards", cards);										//stores data
 		}
 
-		public void checkCards(object sender, EventArgs ars)        //checks if cards are right
-		{
-			answer = answerBox.Text;                                //finds what is in the box
-			int[] c = (int[])ViewState["cards"];                    //gets the cards out of the view
-			int a = c[5];                                           //finds the answer card's value
-			try
-			{
-				computedAnswer = (int)dt.Compute(answer, "");       //turns it into an int
+		public void checkCards(object sender, EventArgs ars) {					//checks if cards are right
+			answer = ("((((((("+ answerBox.Text + ")))))))");                             //finds what is in the box
+			int[] c = (int[])ViewState["cards"];								//gets the cards out of the view
+			int a = c[5];														//finds the answer card's value
+			try {
+				computedAnswer = (int)dt.Compute(answer, "");					//turns it into an int
 			}
-			catch
-			{
+			catch {
 				computedAnswer = -1;
 			}
-			if ((Boolean)ViewState["canGetCards"])
-			{
-				if (computedAnswer == -1)                               //checks if answer is correct
-				{
+			if ((Boolean)ViewState["canGetCards"]) {
+				if (computedAnswer == -1) {                             		//checks if answer is correct
 					label.Text = "ERROR INVALID ANSWER";
 				}
-				else if (computedAnswer != a)
-				{
+				else if (computedAnswer != a) {
 					label.Text = "Answer Does not match";
 				}
-				else if (contains(answer, c))
-				{
+				else if (contains(answer, c)) {
 					label.Text = "Correct";
 					ViewState.Add("canGetCards", false);
-					pointTotal = (int)Session["ScoreTotal"];
-					pointTotal++;
-					Score.InnerText = pointTotal.ToString();
-					Session["ScoreTotal"] = pointTotal;
 				}
 				else {
 					label.Text = "You did not use the cards correctly";
@@ -86,49 +59,44 @@ namespace Krypton
 			}
 		}
 
-		public bool contains(string a, int[] c)                             //not used (yet) ignore
-		{
-			int length = (c.Length - 2);
+		public bool contains(string a, int[] c) {		//checks if the answer contains all of the numbers
+			int length = (c.Length - 2); 				//length of the array minus two (one for the answer, one because arrays start at zero
 
-			for (int i = 0; i < length; i++)
-			{
-				int n = c[i];
-				if (containsNumber(a, n.ToString()))
-				{
-					continue;
+			for (int i = 0; i < length; i++) {			//goes through all cards
+				int n = c[i];							//get current card
+				if (containsNumber(a,n.ToString())) {	//does it contain the number
+					continue;							//yes?  great! keep checking
 				}
 				else {
-					return false;
+					return false;						//no?  :( end the check
 				}
 			}
-			return true;
+			return true;								//If you run through them all, your done, return true
 		}
 
-		public bool containsNumber(string big, string small)
-		{
-			if (big.Contains(small))
-			{
-				int s = big.IndexOf(small, StringComparison.Ordinal) + 1;
-				int e = s + small.Length;
+		public bool containsNumber(string big, string small) {									//checks if a string contains another string
+			if (big.Contains(small)) {															//does it contain the string AND is it not surrounded by another string (2150 != 15)
+				int s = big.IndexOf(small, StringComparison.Ordinal) - 1;						//start of the small string in big string
+				int e = s + small.Length;														//end of the small string in the big string
 
-				if (!isNumber(big.Substring(s, s + 1)) && !isNumber(big.Substring(s, e + 1)))
-				{
-					return true;
+				if (!isNumber(big.Substring(s,s+1)) && !isNumber(big.Substring(e, e + 1))) {	//are the edges not numbers?
+					return true;																//Yay! our number is alone
 				}
 			}
-			return false;
+			return false;																		//else: woops, not right!
 		}
 
-		public bool isNumber(String s)
-		{
-			if (s.Length != 1)
-			{
+		public bool isNumber(String s) {
+			if (s.Length != 1) {
 				return false;
 			}
 			char[] ch = s.ToCharArray();
-			if (ch[0] == '0' || ch[0] == '1' || ch[0] == '2' || ch[0] == '3' || ch[0] == '4' || ch[0] == '5' || ch[0] == '6' || ch[0] == '7' || ch[0] == '8' || ch[0] == '9')
+			for (int i = 0; i < ch.Length; i++)
 			{
-				return true;
+				if (ch[i] == '0' || ch[i] == '1' || ch[i] == '2' || ch[i] == '3' || ch[i] == '4' || ch[i] == '5' || ch[i] == '6' || ch[i] == '7' || ch[i] == '8' || ch[i] == '9')
+				{
+					return true;
+				}
 			}
 			return false;
 		}
